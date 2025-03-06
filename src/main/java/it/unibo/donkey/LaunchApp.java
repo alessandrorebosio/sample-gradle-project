1447f76ffb8dedaaa1d593bf63249fd635b324a9
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-06 10:48:20 +0100

    added mvc pattern

---
+++ src/main/java/it/unibo/donkey/LaunchApp.java
@@ -0,0 +1,17 @@
+package it.unibo.donkey;
+
+/**
+ * Entry point of the app, it creates a controller and launches the GUI
+ * so that the game can start.
+ */
+public class LaunchApp {
+
+    /**
+     * Starts the application.
+     * 
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        System.out.println("Entry Point");
+    }
+}
